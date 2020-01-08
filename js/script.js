507b80e26ea860d6d09febf3d9595aab4e933d2b
tesfaiabay <59484070+tesfaiabay@users.noreply.github.com>
2020-01-08 10:08:24 -0600

    Update script.js
    added w in addr

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
     var address = document.getElementById('streetaddress');
     var phone = document.getElementById('phone');
 
-    address.innerHTML = "417 Roosevelt Rd.<br>Wheaton, IL 60187";
+    address.innerHTML = "417 W Roosevelt Rd.<br>Wheaton, IL 60187";
     phone.innerHTML = "630-698-5611"
 
     // Year
@@ -19,39 +19,6 @@
 
     cy.innerHTML = year;
   });
-
-
-
-
-  //
-  //
-  //
-  //
-  // $(function() { var logo = $(".lrg-logo"); $(window).scroll(function() {
-  // var scroll = $(window).scrollTop();
-  //
-  //     if (scroll >= 500) {
-  //       if(!logo.hasClass("sml-logo")) {
-  //         logo.hide();
-  //         logo.removeClass('lrg-logo').addClass("sml-logo").fadeIn( "slow");
-  //       }
-  //     } else {
-  //       if(!logo.hasClass("lrg-logo")) {
-  //         logo.hide();
-  //         logo.removeClass("sml-logo").addClass('lrg-logo').fadeIn( "slow");
-  //       }
-  //     }
-  //
-  // });
-  // });
-  //
-  //
-  //
-  //
-  //
-  //
-
-
 
 
 })();
